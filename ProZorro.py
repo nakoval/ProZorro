a9c9c96ed8917aa9267bae76ae7d262926fc63cd
nakoval <32037598+nakoval@users.noreply.github.com>
2017-09-17 20:31:32 +0300

    Update ProZorro.py

--- ProZorro.py
+++ ProZorro.py
@@ -6,7 +6,7 @@
 
 import requests
 import json
-response = requests.get('http://api-docs.openprocurement.org/uk_UA/latest/')
+response = requests.get('https://lb.api.openprocurement.org/api/0/tenders/ae5ccc2b1a974a80a294f21190dbd7ed')
 print(response.content)
 result = json.loads(response.json)[0]
 # Вместо ['data']['item'][...] нужно вписать паравильные ключи словаря 
