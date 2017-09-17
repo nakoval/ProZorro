895d7732bd9631f018c1abe4e13ea41dd9de243f
gtipit <gtipit@gmail.com>
2017-09-17 19:52:34 +0300

    Update ProZorro.py
    пример просмотра json

--- ProZorro.py
+++ ProZorro.py
@@ -8,4 +8,7 @@ import requests
 import json
 response = requests.get('http://api-docs.openprocurement.org/uk_UA/latest/')
 print(response.content)
+result = json.loads(response.json)[0]
+# Вместо ['data']['item'][...] нужно вписать паравильные ключи словаря 
+print(result['data'])
 
