9bc0493773979fd77260aa315a4be29bb562574f
gtipit <gtipit@gmail.com>
2017-09-17 20:40:46 +0300

    Update ProZorro.py
    у меня получилось: Державна школа вищої спортивної майстерності

--- ProZorro.py
+++ ProZorro.py
@@ -1,14 +1,15 @@
 
 # coding: utf-8
 
-# In[3]:
-
 
 import requests
 import json
-response = requests.get('https://lb.api.openprocurement.org/api/0/tenders/ae5ccc2b1a974a80a294f21190dbd7ed')
-print(response.content)
-result = json.loads(response.json)[0]
-# Вместо ['data']['item'][...] нужно вписать паравильные ключи словаря 
-print(result['data'])
 
+
+response = requests.get('https://lb.api.openprocurement.org/api/0/tenders/ae5ccc2b1a974a80a294f21190dbd7ed')
+# print(response.content)
+result = json.loads(response.content)
+# Если раскоментировать строку ниже, можно смотреть ключи
+# print result
+# Вместо ['data']['procuringEntity']['name'] нужно вписать паравильные ключи словаря 
+print(result['data']['procuringEntity']['name'])
